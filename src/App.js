cc6dced690ec5b03be3253985bd924e0a19a6d0f
sakout-reda <sakout-reda@hotmail.fr>
2022-02-18 12:58:18 +0100

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
-  const title = 'My Awesome Project';
+/*   const title = 'My Awesome Project';
   const likes = 50;
   const person = {name: 'Reda', age:25};
-  const link = 'https://www.google.com';
+  const link = 'https://www.google.com'; */
   return (
     <div className="App">
+      <Navbar/>
+
       <div className="content">
-        <h1>{title}</h1>
+      <Home/>
+        {/* <h1>{title}</h1>
         <p>Liked {likes} times</p>
         <p>{person.age}</p>
         <p>{person.name}</p>
         <p>{Math.random()}</p>
-        <a href={link}>Google Site</a>
+        <a href={link}>Google Site</a> */}
       </div>
     </div>
   );
