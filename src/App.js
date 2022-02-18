3a41b4ccc42aa821daea5cc05dce62bcdd4f7818
sakout-reda <sakout-reda@hotmail.fr>
2022-02-18 12:19:27 +0100

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = 'My Awesome Project';
+  const likes = 50;
+  const person = {name: 'Reda', age:25};
+  const link = 'https://www.google.com';
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{person.age}</p>
+        <p>{person.name}</p>
+        <p>{Math.random()}</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
