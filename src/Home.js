a88ca361cb540e97f73d637fe23a7c5634e129c0
sakout-reda <sakout-reda@hotmail.fr>
2022-02-19 11:28:43 +0100

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) =>{
+        console.log('Hello World\n', e.target);
+    }
+    const handleClickMeAgain =(name) =>{
+        console.log('Hello Mr. '+ name);
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={()=> handleClickMeAgain('Mario')}>Click Me Again</button>
         </div>
     );
 }
