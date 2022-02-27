af210d0ac8a730fae3f0c921dafb574c0291c2bd
sakout-reda <sakout-reda@hotmail.fr>
2022-02-27 21:23:00 +0100

    Complete

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
 import useFetch from './useFetch';
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, isLoading, myError } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+     const deleteBlog = () =>  {
+        fetch('http://localhost:8000/blogs/'+id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        });
+    }
     return (
         <div className="blog-details">
             {isLoading && <div>Loadding...</div>}
@@ -11,6 +21,7 @@ const BlogDetails = () => {
             {blog && (
                 <article>
                     <h2> {blog.title} </h2>
+                    <button onClick={() => deleteBlog(blog.id)}>Delete Me</button>
                     <p> Wrriten by {blog.author} </p>
                     <div> {blog.body} </div>
                 </article>
--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setisPending] = useState(false);
+    const history = useHistory();
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
@@ -18,6 +20,7 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setisPending(false);
+            history.push('/');
         });
     }
 
